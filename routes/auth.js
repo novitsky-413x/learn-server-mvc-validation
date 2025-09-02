141284bec2d54df57a1294a39d251de0b2f5b6a7
Alexander Novitsky <novitsky.413x@gmail.com>
2025-09-02 17:27:53 +0300

    302. Sanitizing Data

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,8 @@ router.get('/signup', authController.getSignup);
 router.post(
     '/login',
     [
-        body('email').isEmail().withMessage('Please enter a valid email address.'),
-        body('password', 'Password has to be valid.').isLength({ min: 5 }).isAlphanumeric(),
+        body('email').isEmail().withMessage('Please enter a valid email address.').normalizeEmail(),
+        body('password', 'Password has to be valid.').isLength({ min: 5 }).isAlphanumeric().trim(),
     ],
     authController.postLogin
 );
@@ -33,17 +33,20 @@ router.post(
                         return Promise.reject('Email exists already, please pick a different one.');
                     }
                 });
-            }),
-
+            })
+            .normalizeEmail(),
         body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
             .isLength({ min: 5 })
-            .isAlphanumeric(),
-        body('confirmPassword').custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error('Passwords have to match!');
-            }
-            return true;
-        }),
+            .isAlphanumeric()
+            .trim(),
+        body('confirmPassword')
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error('Passwords have to match!');
+                }
+                return true;
+            }),
     ],
 
     authController.postSignup
